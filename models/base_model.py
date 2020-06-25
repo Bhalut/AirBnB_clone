71ea3143a6b586a2f5713f7c0446adefbbe26c47
AndersenCastaneda <andersenpiano@gmail.com>
2020-06-25 22:17:03 +0000

    Add BaseModel first version

---
+++ models/base_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Module BaseModule
+"""
+from uuid import uuid4
+from datetime import datetime
+import time
+
+
+class BaseModel:
+    """BaseModel Class
+    Public instance attributes:
+        id = Type string (unique id)
+        created_ad = Creation date
+        update_ad = Last modification date
+    Public instance methods:
+    """
+    def __init__(self):
+        """Instance Initializer"""
+        self.id = str(uuid4())
+        self.created_ad = datetime.now()
+        self.update_ad = datetime.now()
+
+    def __str__(self):
+        msg = "[{}] ({}) {}".format(BaseModel.__name__, self.id, self.__dict__)
+        print(msg)
+
+    def save(self):
+        """Save last time instance modification"""
+        self.update_ad = datetime.today()
+
+    def to_dict(self):
+        pass
+
+# if __name__ == "__main__":
+#     a = BaseModel()
+#     print(a.id)
+#     print(a.update_ad)
+
+#     time.sleep(2)
+#     a.id = "45"
+#     a.save()
+    # print(a.update_ad)
