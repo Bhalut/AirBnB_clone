7b22d2036351dd60b04928f62848a5c043844e13
Abdel Mejia <bhalut.bear@gmail.com>
2020-06-26 20:16:44 +0000

    save function at file_storage file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,16 +21,40 @@ class FileStorage:
 
     def all(self):
         """Returns the dictionary __objects"""
-        return __objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id"""
-        __objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
+        FileStorage.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
 
     def save(self):
         """serializes __objects to the JSON file"""
-        pass
+        data = self.__to_json_string(
+            [obj.to_dictionary() for obj in FileStorage.__objects])
+        filename = FileStorage.__file_path
+        with open(filename, 'a') as file:
+            file.write(data)
 
     def reload(self):
         """Deserializes the JSON file to __objects"""
         pass
+
+    def __to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries
+        Parameters: list_dictionaries
+        """
+        return json.dumps(list_dictionaries)
+
+    def __from_json_string(self, json_string):
+        """Returns the list of the JSON string representation
+        Parameters:
+            json_string: string info JSON format
+        Raises:
+            TypeError: json_string must be a string
+        """
+        if json_string in [None, ""]:
+            return []
+        elif type(json_string) != str:
+            raise TypeError("json_string must be a string")
+        else:
+            return json.loads(json_string)
