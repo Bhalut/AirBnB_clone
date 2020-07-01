85a852baa211eee6f34323a382881b240c3544ef
Abdel Mejia <bhalut.bear@gmail.com>
2020-07-01 22:18:04 +0000

    add test_pep8 at test_base_model file

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+import unittest
+import pep8
+
+
+class TestBaseModel(unittest.TestCase):
+    def test_base_model_pep8(self):
+        """test_base_pep8 test
+
+        Test pep8 for base_model.py file
+        """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['./models/base_model.py'])
+        self.assertEqual(result.total_errors, 0)
