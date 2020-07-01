451cc589992ac7d954b8709f037cc57074383b52
AndersenCastaneda <andersenpiano@gmail.com>
2020-07-01 01:04:22 +0000

    Add help functions

--- console.py
+++ console.py
@@ -11,9 +11,30 @@ class HBNBCommand(cmd.Cmd):
     """AirBnb Clone"""
     prompt = "(hbnb) "
 
+    def default(self, line):
+        """Process commads:
+            - <class name>.all()
+            - <class name>.show(<id>)
+            - <class name>.destroy(<id>)
+            - <class name>.update(<id>, <attribute name>, <attribute value>)
+            - <class name>.update(<id>, <dictionary representation>)
+        Otherwise print Unknown syntax
+        """
+        message = "*** Unknown syntax in: {}".format(line)
+        # User.show()
+        if "." in line and "(" in line and ")" in line:
+            args = line.split(".")          #User show()
+            c_name = args[0]                #User
+            c_func = args[1].split("(")[0]  #show
+            print(c_func)
+            if c_func in ['all', 'show', 'count', 'update', 'destroy']:
+                return
+
+        print("*** Unknown syntax: {}".format(line))
+
     def do_all(self, line):
         """Prints all string representation of all instances based
-        or not on the class name
+           or not on the class name
         """
         rex = r'^(\S+)(?:\s(\S+)(?:\s(\S+)(?:\s((?:"[^"]*")|(?:(\S)+)))?)?)?'
         match = re.search(rex, line)
@@ -32,8 +53,8 @@ class HBNBCommand(cmd.Cmd):
         print(l_obj)
 
     def do_create(self, line):
-        """Creates a new instance of BaseModel, saves it (to the JSON file)\
-            and prints the id
+        """Creates a new instance, saves it (to the JSON file)
+           and prints the id
         """
         rex = r'^(\S+)(?:\s(\S+)(?:\s(\S+)(?:\s((?:"[^"]*")|(?:(\S)+)))?)?)?'
         match = re.search(rex, line)
@@ -76,6 +97,10 @@ class HBNBCommand(cmd.Cmd):
 
         storage.destroy(key)
 
+    def do_EOF(self, line):
+        """Handles the End Of File"""
+        return True
+
     def do_show(self, line):
         """Prints the string representation of an instance
         based on the class name and id
@@ -102,18 +127,14 @@ class HBNBCommand(cmd.Cmd):
 
         print(storage.all()[key])
 
-    def do_EOF(self, line):
-        """Handles the End Of File"""
-        return True
-
     def do_quit(self, line):
-        """Exit programm"""
+        """Quit command to exit the program\n"""
         return True
 
     def do_update(self, line):
         """Updates an instance based on the class name
         and id by adding or updating attribute
-        (save the change into the JSON file) """
+        (save the change into the JSON file)"""
         rex = r'^(\S+)(?:\s(\S+)(?:\s(\S+)(?:\s((?:"[^"]*")|(?:(\S)+)))?)?)?'
         match = re.search(rex, line)
 
@@ -153,9 +174,51 @@ class HBNBCommand(cmd.Cmd):
         """Handles empty line"""
         return
 
+    # Help Functions
+    def help_all(self):
+        """Print help all instruction"""
+        msg1 = "Prints all string representation of all instances"
+        msg2 = "based or not on the class name."
+        msg3 = "Ex: $ all BaseModel or $ all"
+        print("{} {}\n{}\n".format(msg1, msg2, msg3))
+
+    def help_create(self):
+        """Print help create instruction"""
+        msg1 = "Creates a new instance, saves it(to the JSON file)"
+        msg2 = "and prints the id."
+        msg3 = "Ex: $ create BaseModel"
+        print("{} {}\n{}\n".format(msg1, msg2, msg3))
+
+    def help_destroy(self):
+        """Print help destroy instruction"""
+        msg1 = "Deletes an instance based on the class name"
+        msg2 = "and id (save the change into the JSON file)."
+        msg3 = "Ex: $ destroy BaseModel 1234-1234-1234"
+        print("{} {}\n{}\n".format(msg1, msg2, msg3))
+
+    def help_EOF(self):
+        """Print help EOF instruction"""
+        print("EOF command to exit the program\n")
+
+    def help_show(self):
+        """Print help show instruction"""
+        msg1 = "Prints the string representation of an instance"
+        msg2 = "based on the class name and id."
+        msg3 = "Ex: $ show BaseModel 1234-1234-1234"
+        print("{} {}\n{}\n".format(msg1, msg2, msg3))
+
     def help_quit(self):
-        """Print quit instruction"""
+        """Print help quit instruction"""
         print("Quit command to exit the program\n")
+
+    def help_update(self):
+        """Print help update instruction"""
+        msg1 = "Updates an instance based on the class name"
+        msg2 = "and id by adding or updating attribute"
+        msg3 = "(save the change into the JSON file)."
+        msg4 = "Ex: $ update BaseModel 1234-1234-1234 email"
+        msg5 = '"aibnb@holbertonschool.com"'
+        print("{} {} {}\n{} {}".format(msg1, msg2, msg3, msg4, msg5))
 
 
 if __name__ == '__main__':
