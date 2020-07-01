d8ea6141aacf6509c45011f40455aee6087de98c
Abdel Mejia <bhalut.bear@gmail.com>
2020-07-01 23:30:12 +0000

    add test_dict at test_base_model file

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
+from datetime import datetime
 import os.path as path
 import unittest
 import pep8
@@ -27,6 +28,24 @@ class TestBaseModel(unittest.TestCase):
         model.city = "Medellin"
         model.save()
         self.assertTrue(path.exists("file.json"))
+
+    def test_base_model_dict(self):
+        my_model = BaseModel()
+        my_model.name = "Jerry"
+        my_model.number = 89
+        self.assertIs(type(my_model.id), str)
+        self.assertIs(type(my_model), BaseModel)
+        self.assertIs(type(my_model.created_at), datetime)
+
+        my_model_json = my_model.to_dict()
+        self.assertIs(type(my_model_json), dict)
+
+        my_new_model = BaseModel(**my_model_json)
+        self.assertIs(type(my_new_model.id), str)
+        self.assertIs(type(my_new_model), BaseModel)
+        self.assertIs(type(my_new_model.created_at), datetime)
+
+        self.assertTrue(my_model is not my_new_model)
 
 
 if __name__ == '__main__':
